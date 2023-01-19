334b6d4f61140e8496ea269c871d681e8af852b4
Your Name <izmailcali@gmail.com>
2023-01-19 17:25:44 +0300

    user authentications

--- booksapi/urls.py
+++ booksapi/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('index/',views.index, name = 'index'),
+    path('register/',views.registerPage, name= 'register') ,
+    path('',views.loginPage, name = 'login') ,
+    path('logout/',views.logoutPage, name = 'logout')
+    
 ]
--- booksapi/views.py
+++ booksapi/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import BooK
+from .forms import creationUserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def index(request):
@@ -9,3 +15,38 @@ def index(request):
         'books': books,
     }
     return render(request, 'index.html',context )
+
+
+def registerPage(request):
+    form = creationUserForm()
+    
+    if request.method == 'POST':
+        form = creationUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'account was created '+ user)
+            return redirect('login')
+        
+    context = {'form': form}
+    return render(request, 'registerForm.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate (request, username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'username OR password is incorrect')
+    context = {}
+    return render(request , 'login.html', context)
+    
+def logoutPage(request):
+    logout(request)
+    return redirect ('login') 
+          
