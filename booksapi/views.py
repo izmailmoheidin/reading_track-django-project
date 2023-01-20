8a958c366d58e6319a3ca07a1cae0864461f124d
Your Name <izmailcali@gmail.com>
2023-01-20 19:28:36 +0300

    resticted login

--- booksapi/views.py
+++ booksapi/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     books = BooK.objects.all()
     context = {
